43fc7c418b58db3251d2065c52523f2b06d5bec2
jcbages <jcbages@outlook.com>
2014-09-29 23:40:47 -0500

    first commit

---
+++ cubo/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'cubo.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jcbages/Documents/cubo/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
