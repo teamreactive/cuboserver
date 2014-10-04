d234c8fe924e1a0b7d8c5ff6aba83aeca6311ca5
Sebastián Valencia <sc.valencia606@gmail.com>
2014-10-04 18:32:12 -0500

    Bug fixed

--- proveedores/admin.py
+++ proveedores/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import Proveedor,
-                    ContactosxProveedor
+from .models import Proveedor, ContactosxProveedor
 
 admin.site.register(Proveedor)
 admin.site.register(ContactosxProveedor)
