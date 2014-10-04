1b9f6aab62d20e7161504e04985605fd9f91d63c
imwiwiim90 <wilsondaniel95@hotmail.com>
2014-10-04 16:37:35 -0500

    disabled heroku integration

--- clientes/models.py
+++ clientes/models.py
@@ -8,5 +8,5 @@ class Cliente(models.Model):
     codigo_verificacion = models.CharField(max_length=25, null=True)
     razon_social = models.CharField(max_length=25)
     sigla = models.CharField(max_length=10, null=True)
-    lugar = models.ForeignKey(Lugar, related_name='Cliente.lugar', null=True)
-    logo = models.FileField(upload_to='logos', null=True)
+    lugar = models.ForeignKey(Lugar, related_name='cliente', null=True)
+    logo = models.ImageField(upload_to='logos', null=True)
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/jcbages/Documents/cubo/venv"
+set -gx VIRTUAL_ENV "/home/wilsond/Documentos/cubo/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
