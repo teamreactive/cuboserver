718f5c51dac86b4894d67cfbfe97727a055f21e7
Sebastián Valencia <sc.valencia606@gmail.com>
2014-10-04 21:02:51 -0500

    Style fixed

--- clientes/models.py
+++ clientes/models.py
@@ -1,12 +1,9 @@
 from django.db import models
 
-#from lugares.models import Lugar
-
 
 class Cliente(models.Model):
     nit = models.CharField(max_length=25)
     codigo_verificacion = models.CharField(max_length=25, null=True)
     razon_social = models.CharField(max_length=25)
     sigla = models.CharField(max_length=10, null=True)
-#    lugar = models.ForeignKey(Lugar, related_name='Cliente', null=True, on_delete=models.PROTECT)
     logo = models.ImageField(upload_to='logos', null=True)
