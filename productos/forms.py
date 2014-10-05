e465ddd69368ebc9156a36315986820b9e740669
jcbages <jcbages@outlook.com>
2014-10-05 10:36:13 -0500

    crear producto added

---
+++ productos/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from .models import Producto
+
+
+class ProductoForm(ModelForm):
+    class Meta:
+        model = Producto
+        fields = [
+            'codigo_onu', 'nombre', 'descripcion',
+            'referencia', 'marca', 'servicio',
+            'familia_proveedor', 'tiempo_promedio',
+            'cliente', 'equipo', 'centro_de_costo'
+        ]
