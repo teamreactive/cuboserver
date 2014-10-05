85d7744fa0a0715768a76a8000c7e90991c979fa
imwiwiim90 <wilsondaniel95@hotmail.com>
2014-10-05 10:51:21 -0500

    removed table

--- familiasproveedores/models.py
+++ familiasproveedores/models.py
@@ -7,7 +7,4 @@ from contactos.models import Contacto
 class FamiliaProveedor(models.Model):
     nombre = models.CharField(max_length=25)
     cliente = models.ForeignKey(Cliente)
-
-class ContactosxFamiliaProveedor(models.Model):
-    familia_proveedor = models.ForeignKey(FamiliaProveedor)
-    contactos = models.ForeignKey(Contacto)
+    contactos = models.ManyToManyField(Contacto,db_table='familiaproveedorxcontacto')
