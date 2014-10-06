8d83e94208069076fd0d15fdc869d2ee281fc427
imwiwiim90 <wilsondaniel95@hotmail.com>
2014-10-05 20:03:51 -0500

    mostly final changes at 800pm

--- lugares/models.py
+++ lugares/models.py
@@ -5,19 +5,9 @@ from usuarios.models import Usuario
 
 
 class Lugar(models.Model):
-    SECCIONES = (
-        ('1', 'calle'),
-        ('2', 'carrera'),
-        ('3', 'transversal'),
-        ('4', 'avenida'),
-        ('5', 'diagonal'),
-    )
     nombre = models.CharField(max_length=25, null=True, blank=True)
-    seccion = models.CharField(max_length=1, choices=SECCIONES)
-    numero_seccion = models.CharField(max_length=5)
-    numero_1 = models.CharField(max_length=5)
-    numero_2 = models.CharField(max_length=5)
-    extra = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=25)
+    descripcion = models.CharField(max_length=100)
     usuario = models.ForeignKey(Usuario, null=True)
     contactos = models.ManyToManyField(Contacto, db_table='lugarxcontactos')
 
