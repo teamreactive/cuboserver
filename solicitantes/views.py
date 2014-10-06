846e0ac178805cae606e827ea90bdc857c0c1593
jcbages <jcbages@outlook.com>
2014-10-06 12:36:05 -0500

    bootstrap !

--- solicitantes/views.py
+++ solicitantes/views.py
@@ -1,35 +1,50 @@
 from django.shortcuts import render, redirect
 
 from lugares.forms import LugarForm
-from productos.forms import ProductoForm
+from productos.forms import ProductoForm, FotosxProductoForm
 from solicitudes.forms import SolicitudForm
+from solicitudes.models import Solicitud
+from usuarios.models import Usuario
 from usuarios.views import get_usuario
 
 
 def inicio_solicitante(request):
     usuario = get_usuario(request)
     if usuario and usuario.tipo == '3':
-        solicitud_form = SolicitudForm()
-        lugar_form = LugarForm()
-        producto_form = ProductoForm()
+        solicitudes = Solicitud.objects.filter(solicitante=usuario)
         if request.method == 'POST':
+            solicitud_form = SolicitudForm(request.POST)
+            lugar_form = LugarForm()
+            producto_form = ProductoForm()
+            fotosxproducto_form = FotosxProductoForm()
             if solicitud_form.is_valid():
-                solicitud_form.save()
+                solicitud = solicitud_form.save(commit=False)
+                solicitud.estado = '1' if len(Usuario.objects.filter(tipo='5')) > 0 else '3'
+                solicitud.solicitante = usuario
+                solicitud.save()
                 return redirect('/inicio_solicitante/')
             else:
-                solicitud_form = SolicitudForm(solicitud_form.cleaned_data)
                 params = {
                     'usuario': usuario,
                     'solicitud_form': solicitud_form,
                     'lugar_form': lugar_form,
                     'producto_form': producto_form,
+                    'fotosxproducto_form': fotosxproducto_form,
+                    'solicitudes': solicitudes,
                 }
+                return render(request, 'solicitantes/inicio_solicitante.html', params)
         else:
+            solicitud_form = SolicitudForm()
+            lugar_form = LugarForm()
+            producto_form = ProductoForm()
+            fotosxproducto_form = FotosxProductoForm()
             params = {
                 'usuario': usuario,
                 'solicitud_form': solicitud_form,
                 'lugar_form': lugar_form,
                 'producto_form': producto_form,
+                'fotosxproducto_form': fotosxproducto_form,
+                'solicitudes': solicitudes,
             }
             return render(request, 'solicitantes/inicio_solicitante.html', params)
     else:
