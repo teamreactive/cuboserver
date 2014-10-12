423878b8e7a5597c9512d96561eb835d69a9def4
imwiwiim90 <wilsondaniel95@hotmail.com>
2014-10-12 00:09:04 -0500

    generate form

--- compradores/views.py
+++ compradores/views.py
@@ -3,6 +3,11 @@ from usuarios.views import get_usuarios
 
 
 def inicio_comprador(request):
-    pass
+    usuario = get_usuario(request)
+    if usuario and usuario.tipo == '6':
+        pass
+    else:
+        return redirect('/')
+
 
 # Create your views here.
