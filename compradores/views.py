0aef38a33adb0f0ff9262234e89e5a1f4434030c
imwiwiim90 <wilsondaniel95@hotmail.com>
2014-10-11 23:17:53 -0500

    think fix error

--- compradores/views.py
+++ compradores/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from usuarios.views import get_usuarios
 
-def inicio_comprador(request):
 
+def inicio_comprador(request):
+    pass
 
 # Create your views here.
